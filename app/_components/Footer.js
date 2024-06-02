444b6b6e13b8dab43a3722b017750e7e3f73e695
radegast0 <fatih.yonucuoglu@gmail.com>
2024-06-02 03:38:30 +0300

    footer layout

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="mx-auto flex max-w-[1150px] flex-col gap-24 px-8">
+    <div className="mx-auto flex max-w-[1150px] flex-col gap-12 px-8">
       <div className="border-b-2 border-light/30 py-12">
         <div className="grid grid-cols-1 justify-between gap-4 small:grid-cols-2">
           <div className="flex flex-col gap-4">
@@ -29,13 +29,37 @@ const Footer = () => {
               ediyor ve beije&apos;den haber almayı onaylıyorum.
             </p>
           </div>
-          <div className="flex flex-col gap-4">
-            <h1>beije Ped</h1>
-            <h1>beije Günlük Ped</h1>
+          <div className="flex flex-row justify-between text-sm">
+            <div className="flex flex-col gap-4">
+              <h1>beije Ped</h1>
+              <h1>beije Günlük Ped</h1>
+              <h1>beije Tampon</h1>
+              <h1>beije Store</h1>
+            </div>
+            <div className="flex flex-col gap-4 text-end small:text-start">
+              <h1>Blog</h1>
+              <h1>Biz Kimiz?</h1>
+              <h1>Sıkça Sorulan Sorular</h1>
+              <h1>Quiz</h1>
+            </div>
+            <div className="hidden flex-col gap-4 small:flex">
+              <h1>Facebook</h1>
+              <h1>Instagram</h1>
+              <h1>Twitter</h1>
+              <h1>LinkedIn</h1>
+              <h1>Spotify</h1>
+            </div>
+          </div>
+          <div className="mt-4 flex flex-row justify-between text-xs small:hidden">
+            <h1>Facebook</h1>
+            <h1>Instagram</h1>
+            <h1>Twitter</h1>
+            <h1>LinkedIn</h1>
+            <h1>Spotify</h1>
           </div>
         </div>
       </div>
-      <div>2</div>
+      <div className="pb-12 opacity-50">2024 beije. Tüm hakları saklıdır.</div>
     </div>
   );
 };
